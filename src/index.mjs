6f439b91266eb8579fd2b5453661a686b79db68b
dweb <developersiteweb@gmail.com>
2026-05-11 22:05:11 +0200

    Implement authentication routes and mock user data for login functionality

--- src/index.mjs
+++ src/index.mjs
@@ -2,6 +2,7 @@ import express from 'express';
 import routes from './routes/index.mjs';
 import cookieParser from 'cookie-parser';
 import session from 'express-session';
+import { mockUsers } from "./mocks/mock-users.mjs";
 
 export const app = express();
 
@@ -31,6 +32,26 @@ app.get('/', loggingMiddleware, (req, res) => {
     res.cookie('hello', 'world', { maxAge: 60 * 60 * 1000, signed: true });
     res.status(200).send({ msg: 'Hello World!' });
 });
+
+app.post('/api/auth', (req, res) => {
+    const { body: { name, password } } = req;
+    console.log(name, password);
+    const findUser = mockUsers.find((user) => user.name === name);
+    if (!findUser || findUser.password !== password) return res.status(401).send('Bad credentials');
+
+    req.session.user = findUser;
+    return res.status(200).send(findUser);
+});
+
+app.get('/api/auth/status', (req, res) => {
+    console.log(req.session.user);
+    req.sessionStore.get(req.sessionID, (err, session) => {
+        console.log(session);
+    })
+    return req.session.user
+        ? res.status(200).send({ authenticated: true, user: req.session.user })
+        : res.status(401).send('Unauthorized');
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- src/mocks/mock-users.mjs
+++ src/mocks/mock-users.mjs
@@ -1,6 +1,6 @@
 export const mockUsers = [
-    { id: 1, name: 'John Doe', email: 'some@email.com' },
-    { id: 2, name: 'John', email: 'john@email.com' },
-    { id: 3, name: 'Bill', email: 'billg@gmail.com' },
-    { id: 4, name: 'Billie', email: 'billie@gmail.com' },
+    { id: 1, name: 'John Doe', email: 'some@email.com', password: '123456' },
+    { id: 2, name: 'john', email: 'john@email.com', password: '123456' },
+    { id: 3, name: 'Bill', email: 'billg@gmail.com', password: '123456' },
+    { id: 4, name: 'Billie', email: 'billie@gmail.com', password: '123456' },
 ];
