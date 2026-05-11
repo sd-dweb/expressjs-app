3672554906e0a4ac8c7c797b6db8aaf27342691b
dweb <developersiteweb@gmail.com>
2026-05-11 09:19:26 +0200

    Add input validation using express-validator for user routes

--- src/index.mjs
+++ src/index.mjs
@@ -1,4 +1,5 @@
-import express from 'express';
+import express, {request} from 'express';
+import { query, validationResult, body, matchedData } from "express-validator";
 
 export const app = express();
 
@@ -39,7 +40,10 @@ app.get('/', loggingMiddleware, (req, res) => {
     res.status(200).send({ msg: 'Hello World!' });
 });
 
-app.get('/api/users', (req, res) => {
+app.get('/api/users',
+    query('filter ').isString().notEmpty()
+    .isLength({ min: 3, max: 10 }).withMessage("Must be at least 3-10 characters"), (req, res) => {
+    const result = validationResult(req);
     const { query: { filter, value } } = req;
 
     if (filter && value) return res.send(
@@ -49,14 +53,24 @@ app.get('/api/users', (req, res) => {
     return res.send(mockUsers);
 });
 
-app.post('/api/users', (req, res) => {
-    console.log(req.body);
-    const { body } = req;
-    const newUser = { id: mockUsers[mockUsers.length - 1].id + 1, ...body };
-    mockUsers.push(newUser);
+app.post('/api/users',
+    body('username')
+        .notEmpty().withMessage("Username cannot be empty")
+        .isLength({ min: 3, max: 10 }).withMessage("Must be at least 3-10 characters"),
+    body('displayName').isEmpty(),
+    (req, res) => {
+        const result = validationResult(req);
 
-    return res.status(201).send(newUser);
-})
+        if(!result.isEmpty())
+            return res.send(400).send({ errors: result.array() });
+
+        const data = matchedData(request);
+        const newUser = { id: mockUsers[mockUsers.length - 1].id + 1, ...data };
+        mockUsers.push(newUser);
+
+        return res.status(201).send(newUser);
+    }
+)
 
 app.get('/api/users/:id', resolveIndexById, (req, res) => {
     const { findUserIndex } = req;
