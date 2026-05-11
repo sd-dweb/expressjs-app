481ec570ce1ab7b27febf97345ed20267f008449
dweb <developersiteweb@gmail.com>
2026-05-11 15:11:28 +0200

    Add cookie-parser middleware and implement cookie-based access control for product routes

--- src/index.mjs
+++ src/index.mjs
@@ -1,9 +1,11 @@
 import express from 'express';
 import routes from './routes/index.mjs';
+import cookieParser from 'cookie-parser';
 
 export const app = express();
 
 app.use(express.json());
+app.use(cookieParser('secret'));
 app.use(routes);
 
 const loggingMiddleware = (req, res, next) => {
@@ -14,6 +16,7 @@ const loggingMiddleware = (req, res, next) => {
 const PORT = process.env.PORT || 3000;
 
 app.get('/', loggingMiddleware, (req, res) => {
+    res.cookie('hello', 'world', { maxAge: 60 * 60 * 1000, signed: true });
     res.status(200).send({ msg: 'Hello World!' });
 });
 
--- src/routes/products.mjs
+++ src/routes/products.mjs
@@ -3,7 +3,13 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/api/products', (req, res) => {
-    res.send([{ id: 123, name: 'iphone', price: 1200.00 }]);
+    console.log(req.headers.cookie);
+    console.log(req.cookies);
+    console.log(req.signedCookies.hello);
+    if (req.signedCookies.hello && req.signedCookies.hello === 'world') {
+        return res.send([{ id: 123, name: 'iphone', price: 1200.00 }]);
+    }
+   return res.status(403).send({msg: "sorry. you need the correct cookie"})
 })
 
 export default router;
