6bca81b8b7f163ffac1afe8ad503bea15788205c
dweb <developersiteweb@gmail.com>
2026-05-14 14:01:44 +0200

    Add Jest configuration and unit tests for password hashing utilities

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+export default {
+  testEnvironment: 'node',
+  transform: {},
+  moduleFileExtensions: ['js', 'mjs'],
+  testMatch: ['**/__tests__/**/*.mjs', '**/?(*.)+(spec|test).mjs'],
+  collectCoverageFrom: [
+    'src/**/*.mjs',
+    '!src/index.mjs',
+    '!src/mocks/**'
+  ],
+  coverageDirectory: 'coverage',
+  verbose: true
+};
+
