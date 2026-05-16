47cf9f4c18e3258b3392edbc1cecd773df92b4c3
dweb <developersiteweb@gmail.com>
2026-05-16 19:24:00 +0200

    Refactor app initialization to connect to MongoDB before starting the server

--- src/index.mjs
+++ src/index.mjs
@@ -1,15 +1,18 @@
 import 'dotenv/config';
+import mongoose from 'mongoose';
 import { createApp } from "./createApp.mjs";
-
-export const app = createApp()
-
-mongoose.connect('mongodb://localhost:27017/express_db')
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.log('Error', err));
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+mongoose.connect('mongodb://localhost:27017/express_db')
+  .then(() => {
+    console.log('Connected to MongoDB');
+
+    const app = createApp();
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch(err => console.log('Error', err));
 
