ff5ad758d8916b20b9329be7ff2682e1fafb5524
dweb <developersiteweb@gmail.com>
2026-05-20 11:51:06 +0200

    Replace mock user data with database queries for user retrieval and deletion

--- src/routes/users.mjs
+++ src/routes/users.mjs
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { query, checkSchema } from 'express-validator';
 import { mockUsers } from '../mocks/mock-users.mjs';
+import { User } from '../schemas/user.mjs';
 import { createUserValidationSchema } from '../utils/validationSchemas.mjs';
 import { resolveIndexById } from '../utils/middlewares.mjs';
 import { createUserHandler } from '../handlers/users.mjs';
@@ -9,20 +10,19 @@ const router = Router();
 
 router.get('/api/users',
   query('filter').isString().notEmpty()
-    .isLength({ min: 3, max: 10 }).withMessage('Must be at least 3-10 characters'), (req, res) => {
-    console.log(req.user);
-    console.log(req.session);
-    console.log(req.session.id);
-    req.sessionStore.get(req.session.id, (err, sessionData) => {
-      console.log('sessionData', sessionData);
-    });
+    .isLength({ min: 3, max: 10 }).withMessage('Must be at least 3-10 characters'), async (req, res) => {
+    // console.log(req.user);
+    // console.log(req.session);
+    // console.log(req.session.id);
+    // req.sessionStore.get(req.session.id, (err, sessionData) => {
+    //   console.log('sessionData', sessionData);
+    // });
     const { query: { filter, value } } = req;
 
-    if (filter && value) return res.send(
-      mockUsers.filter((user) => user[filter].includes(value)),
-    );
+    const query = filter && value ? { [filter]: { $regex: value, $options: 'i' } } : {};
+    const users = await User.find(query);
 
-    return res.send(mockUsers);
+    return res.send(users);
   },
 );
 
@@ -65,10 +65,11 @@ router.patch('/api/users/:id', resolveIndexById, (req, res) => {
   return res.status(200).send(mockUsers[findUserIndex]);
 });
 
-router.delete('/api/users/:id', resolveIndexById, (req, res) => {
-  const { findUserIndex } = req;
+router.delete('/api/users/:id', async (req, res) => {
+  const { params: { id } } = req;
 
-  mockUsers.splice(findUserIndex, 1);
+  const deletedUser = await User.findByIdAndDelete(id);
+  if (!deletedUser) return res.status(404).send({ msg: 'User not found!' });
 
   return res.status(200).send({ msg: 'User deleted successfully!' });
 });
